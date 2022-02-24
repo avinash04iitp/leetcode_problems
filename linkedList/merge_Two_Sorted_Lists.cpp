979ba794a488e6e1ca08047f02b1e12a3dc1a732
Avinash Kumar <46194822+avinash04iitp@users.noreply.github.com>
2022-02-25 00:08:45 +0530

    Create merge_Two_Sorted_Lists.cpp

---
+++ linkedList/merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,46 @@
+// Ref sol:- https://leetcode.com/explore/learn/card/linked-list/213/conclusion/1227/discuss/9715/Java-1-ms-4-lines-codes-using-recursion
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+Example 1:
+    Input: list1 = [1,2,4], list2 = [1,3,4]
+    Output: [1,1,2,3,4,4]
+Example 2:
+    Input: list1 = [], list2 = []
+    Output: []
+Example 3:
+    Input: list1 = [], list2 = [0]
+    Output: [0]
+Constraints:
+    The number of nodes in both lists is in the range [0, 50].
+    -100 <= Node.val <= 100
+    Both list1 and list2 are sorted in non-decreasing order.
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+    {
+        if(l1 == NULL) return l2;
+		if(l2 == NULL) return l1;
+		if(l1->val < l2->val){
+			l1->next = mergeTwoLists(l1->next, l2);
+			return l1;
+		} else{
+			l2->next = mergeTwoLists(l1, l2->next);
+			return l2;
+		}
+    }
+};
